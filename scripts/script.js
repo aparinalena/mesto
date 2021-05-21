0c8d029f1062596a969ae4283122b600f3cd0fc7
aparinalena <yelenaaparina@yandex.ru>
2021-05-21 21:44:44 +0300

    mesto/third-check

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,21 @@
 let openPopup = document.querySelector('.popup-open');
 let popup = document.querySelector('.popup');
 let closePopup = document.querySelector('.popup__close');
-
-function popupOpener () {
-    popup.classList.add('popup_opened');
-    nameInput.value = document.querySelector('.profile__name').textContent;
-    jobInput.value = document.querySelector('.profile__job').textContent;
-}
-
-openPopup.addEventListener('click', popupOpener);
-
-function popupRemover () {
-    popup.classList.remove('popup_opened');
-}
-
-closePopup.addEventListener('click', popupRemover);
-
 let formElement = document.querySelector('form');
 let nameInput = document.querySelector('.popup__item_el_name');
 let jobInput = document.querySelector('.popup__item_el_job');
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__job');
+
+function popupOpener () {
+    popup.classList.add('popup_opened');
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+}
+
+function popupRemover () {
+    popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
@@ -29,4 +24,6 @@ function formSubmitHandler (evt) {
     popupRemover();
 }
 
+openPopup.addEventListener('click', popupOpener);
+closePopup.addEventListener('click', popupRemover);
 formElement.addEventListener('submit', formSubmitHandler); 
