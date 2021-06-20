d1c75dc4d5c10c7c2cda7e9f80a5e671cab13c59
aparinalena <yelenaaparina@yandex.ru>
2021-06-20 16:00:21 +0300

    refactor: вынести initial-cards в отдельный script

--- scripts/index.js
+++ scripts/index.js
@@ -20,33 +20,6 @@ const popupImageElement = document.querySelector('.popup__image');
 
 const closeButtons = document.querySelectorAll('.popup__close');
 
-const initialCards = [
-    {
-      name: 'Токио',
-      link: './images/tokyo.jpg'
-    },
-    {
-      name: 'Киото',
-      link: './images/kyoto.jpg'
-    },
-    {
-      name: 'Гора Фудзи',
-      link: './images/mount-fuji.jpg'
-    },
-    {
-      name: 'Храмы Фусими Инари',
-      link: './images/fushimi.jpg'
-    },
-    {
-      name: 'Храм Тенрю-дзи',
-      link: './images/tenryu-ji.jpg'
-    },
-    {
-      name: 'Тории храма Хаконе',
-      link: './images/torii-gate.jpg'
-    }
-  ]; 
-
 const elementTemplate = document.querySelector('#element-template').content;
 const elementsItems = document.querySelector('.elements');
 
---
+++ scripts/initial-cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+        name: 'Токио',
+        link: './images/tokyo.jpg'
+    },
+    {
+        name: 'Киото',  
+        link: './images/kyoto.jpg'
+    },
+    {
+        name: 'Гора Фудзи',
+        link: './images/mount-fuji.jpg'
+    },
+    {
+        name: 'Храмы Фусими Инари',
+        link: './images/fushimi.jpg'
+    },
+    {
+        name: 'Храм Тенрю-дзи',
+        link: './images/tenryu-ji.jpg'
+    },
+    {
+        name: 'Тории храма Хаконе',
+        link: './images/torii-gate.jpg'
+    }
+]; 
